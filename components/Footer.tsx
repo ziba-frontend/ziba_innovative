118ef5538e717018c4aa468a49d6f436dffc5522
bertin <iradukundabertin082@gmail.com>
2024-12-11 16:53:17 +0200

    modify footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -41,22 +41,24 @@ const Footer = () => {
 
    return (
       <>
-         <div className="container p-6 flex flex-col items-center text-center justify-center md:flex-row md:justify-between bg-main text-white relative rounded-t-[40px] z-10 bevel-footer">
-            
-            <h2 className="w-full md:w-[30%] mb-4 md:mb-0 text-center md:text-left">
-               Let’s discuss and bring your vision to life.
-            </h2>
-            <Link href="/contact">
-               <Button className="bg-submain p-6 rounded-full flex items-center justify-center text-white">
-                  Let’s Talk <ArrowRight />
-               </Button>
-            </Link>
+         <div className="container p-6 flex flex-col items-center text-center justify-center md:justify-between bg-main text-white relative rounded-t-[40px] z-10  w-[90%] sm:w-[95%] ">
+            <div className="p-6 flex flex-col items-center text-center justify-center md:justify-between md:flex-row w-full xl:w-[94%] border-b border-input">
+               <h2 className="w-full md:w-[50%] mb-4 md:mb-0 text-center md:text-left">
+                  Let’s discuss and bring your vision to life.
+               </h2>
+               <Link href="/contact">
+                  <Button className="bg-submain p-6 rounded-full flex items-center justify-center text-white">
+                     Let’s Talk <ArrowRight />
+                  </Button>
+               </Link>
+            </div>
          </div>
 
-         <div className="bg-main p-6 text-white relative min-h-[70vh] flex flex-col items-center z-[5]">
-  
+         <div className="bg-main p-6 text-white relative min-h-[70vh] flex flex-col items-center z-[5] ">
+            <div className="bg-white xl:p-6 w-3 sm:w-6 2xl:w-[89px] h-8 rounded-br-[40px] absolute top-0 left-0 z-10"></div>
+            <div className="bg-white xl:p-6 w-3 sm:w-6 2xl:w-[88px] h-8 rounded-bl-[40px] absolute top-0 right-0 z-10"></div>
 
-            <div className="container mx-auto px-4 border-b border-input py-6 flex flex-col items-center sm:flex-row sm:justify-between sm:items-start text-center sm:text-left border-t lg:py-20 ">
+            <div className="container mx-auto px-4 border-b border-input py-6 flex flex-col items-center sm:flex-row sm:justify-between sm:items-start text-center sm:text-left  lg:py-20 ">
                <div className="flex flex-col items-center sm:items-start justify-center w-full lg:w-[40%] xl:w-[55%] 2xl:w-[60%] z-10 ">
                   <div className="flex flex-col items-center sm:items-start gap-3">
                      <Link
